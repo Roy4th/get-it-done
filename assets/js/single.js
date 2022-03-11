57c149a3951a533a69bfc2cfe7226f7c2db11d8c
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-03-11 00:36:31 -0600

    getreponame and link to pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     displayWarning(repo);
 
@@ -75,4 +88,6 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues();
+
+getRepoName();
